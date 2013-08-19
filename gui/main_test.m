cc501d33c1543744f64aa7fbe9ef04af6704cdc7
janbrue <jan.bruemmerstedt@gmx.de>
2013-08-19 17:15:33 +0200

    Input from .mat-files & 1st draft of GUI
    -aux velocity fcn to read in marker positions from .mat-files (instead
    of .c3d)
    - a little test script
    - (very) 1st draft of GUI

---
+++ gui/main_test.m
@@ -0,0 +1,30 @@
+% Test Script for Input routine from .mat-file
+close all, clear all;
+
+% path to the original babytools
+addpath('../matlab/original/');
+
+% C3D Qualisys
+%[raw_c3d,markernames,units] = baby_load_c3d(FETCH_c3dfil);
+fname = 'N30_04';
+load(['../data/' fname]);  
+fps = 60;
+markerindex = 2;    % 1...32 
+smoothpnts = 5;
+speed = aux_velocity(N30_04,fps,markerindex,smoothpnts);
+
+% NOTE
+% IGNORE for now! correction factor for miscalibrated data
+% wand = FETCH_wand;
+% speed = speed / wand;
+raw_qualisys = baby_qualisys_speed(speed);
+
+
+% USER params: set default here
+minvelocity = 200;
+minpauseframes = 12;
+minpeaksinunit = 1;
+minunitframes = 12;
+% %seq_qs = baby_velocitypeaks(raw_qualisys,minvelocity,minpauseframes,minpeaksinunit,minunitframes); % states: 1:false, 2:true
+ [seq_qs,qs_statenames] = baby_velocitypeaks(raw_qualisys,minvelocity,minpauseframes,minpeaksinunit,minunitframes);
+ [seq_qs,qs_statenames] = baby_seq_statenames_order(seq_qs,qs_statenames,{'','x'},{'^\s*$'},{'[xX]'});
